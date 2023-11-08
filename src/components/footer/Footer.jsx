e38a5ada59304d06be9c8d8d7662f74c0106aa11
mjidelouss <mjid.elouss@gmail.com>
2023-11-08 18:41:25 +0100

    Footer...(Done)

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
 import './footer.css'
+import {FaFacebookF} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+import {FaXTwitter} from 'react-icons/fa6'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#" className='footer__logo'>ELOUSSE</a>
+
+      <ul className='permalinks'>
+        <li><a href="#">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#experience">Experience</a></li>
+        <li><a href="#services">Services</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+
+      <div className="footer__socials">
+        <a href="https://www.facebook.com/"><FaFacebookF/></a>
+        <a href="https://www.instagram.com/"><FiInstagram/></a>
+        <a href="https://www.twitter.com/"><FaXTwitter/></a>
+      </div>
+
+      <div className="footer__copyright">
+        <small>&copy; ELOUSSE ABDELMAJID. All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 

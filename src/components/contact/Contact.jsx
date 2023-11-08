6c9f6a115b3827e6438285e866a4cd478feee23f
mjidelouss <mjid.elouss@gmail.com>
2023-11-08 17:08:57 +0100

    Portfolio Part ...(Done -_- 100%)

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,9 +1,34 @@
 import React from 'react'
 import './contact.css'
+import {MdOutlineEmail} from 'react-icons/md'
+import {BsWhatsapp} from 'react-icons/bs'
 
 const Contact = () => {
   return (
-    <section id='contact'>Contact</section>
+    <section id='contact'>
+      <h5>Get In Touch</h5>
+      <h2>Contact Me</h2>
+
+      <div className="container contact__container">
+        <div className="contact__options">
+          <article className='contact__option'>
+            <MdOutlineEmail/>
+            <h4>Email</h4>
+            <h5>mjid.elouss@gmail.com</h5>
+            <a href="mailto:mjid.elouss@gmail.com">Send a Message</a>
+          </article>
+
+          <article className='contact__option'>
+            <BsWhatsapp/>
+            <h4>Whatsapp</h4>
+            <h5>+212636934470</h5>
+            <a href="https://api.whatsapp.com/send?phone=0636934470">Send a Message</a>
+          </article>
+        </div>
+        {/* End Of Contact Options */}
+        <form action=""></form>
+      </div>
+    </section>
   )
 }
 
